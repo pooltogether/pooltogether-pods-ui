b29836d138375313ab529807daecd2227f55e011
Charles Bergeron <chuck.bergeron@gmail.com>
2021-05-26 17:58:12 -0700

    * forgot pages dir

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,84 +1,82 @@
-import { useEthers } from "@usedapp/core";
-import { ErrorBoundary } from "@components";
+import { useEthers } from '@usedapp/core'
+import { ErrorBoundary } from '@components'
 
-import { PodCardAPI } from "@views/PodCardAPI";
+import { PodCardAPI } from '@views/PodCardAPI'
 
 /**
  * @name PageDeposit
  * @param {Object} props
  */
 export const PageDeposit = (props) => {
-  const ethersContext = useEthers();
+  const ethersContext = useEthers()
   // console.log(ethersContext, "ethersContext");
 
   return (
-    <div className="py-20">
-      <div className="text-center text-center mb-10">
-        <h2 className="font-black text-4xl lg:text-6xl text-teal-600">
-          Deposit
-        </h2>
-        <p className="text-1xl text-white">
+    <div className='py-20'>
+      <div className='text-center text-center mb-10'>
+        <h2 className='font-black text-4xl lg:text-6xl text-teal-600'>Deposit</h2>
+        <p className='text-1xl text-white'>
           Deposit with low gas fee and have a chance to win the prize.
         </p>
       </div>
-      <div className="max-w-screen-lg mx-auto w-full p-0 lg:p-0">
+      <div className='max-w-screen-lg mx-auto w-full p-0 lg:p-0'>
         <ErrorBoundary>
           <PodsFromConnectedNetwork />
         </ErrorBoundary>
       </div>
     </div>
-  );
-};
+  )
+}
 
 const PodsFromConnectedNetwork = (props) => {
-  const { chainId } = useEthers();
+  const { chainId } = useEthers()
   switch (chainId) {
     case 1:
     case 1337:
       return (
         <>
           <PodCardAPI
-            classNameContainer="border-solid border-t-4 border-yellow-400"
-            token="DAI"
-            symbol="DAI"
-            tokenSymbol="DAI"
+            classNameContainer='border-solid border-t-4 border-yellow-400'
+            token='DAI'
+            symbol='DAI'
+            tokenSymbol='DAI'
             decimals={18}
-            tokenImage="/tokens/token-dai.png"
+            tokenImage='/tokens/token-dai.png'
           />
           <PodCardAPI
-            classNameContainer="border-solid border-t-4 border-blue-400 mt-10"
-            token="USDC"
-            symbol="USDC"
-            tokenSymbol="USDC"
+            classNameContainer='border-solid border-t-4 border-blue-400 mt-10'
+            token='USDC'
+            symbol='USDC'
+            tokenSymbol='USDC'
             decimals={6}
-            tokenImage="/tokens/token-usdc.png"
+            tokenImage='/tokens/token-usdc.png'
           />
         </>
-      );
+      )
     case 4:
       return (
         <>
           <PodCardAPI
-            classNameContainer="border-solid border-t-4 border-yellow-400"
-            token="DAI"
-            symbol="DAI"
-            tokenSymbol="DAI"
+            classNameContainer='border-solid border-t-4 border-yellow-400'
+            token='DAI'
+            symbol='DAI'
+            tokenSymbol='DAI'
             decimals={18}
-            tokenImage="/tokens/token-dai.png"
+            tokenImage='/tokens/token-dai.png'
           />
           <PodCardAPI
-            classNameContainer="border-solid border-t-4 border-blue-400 mt-10"
-            token="USDC"
-            symbol="USDC"
-            tokenSymbol="USDC"
+            classNameContainer='border-solid border-t-4 border-blue-400 mt-10'
+            token='USDC'
+            symbol='USDC'
+            tokenSymbol='USDC'
             decimals={6}
-            tokenImage="/tokens/token-usdc.png"
+            tokenImage='/tokens/token-usdc.png'
           />
         </>
-      );
+      )
     default:
-      return null;
+      return null
   }
-};
+}
 
-export default PageDeposit;
+export default PageDeposit
